dfde124045ec5dbcc8276c810c7c0b5721ab58b9
Dalton Schwanke <schw1057@stthomas.edu>
2025-03-13 20:37:45 -0500

    added in client components and services

---
+++ src/app/components/featured-blogs/featured-blogs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-blogs',
+  standalone: true,
+  imports: [],
+  templateUrl: './featured-blogs.component.html',
+  styleUrl: './featured-blogs.component.css'
+})
+export class FeaturedBlogsComponent {
+
+}
